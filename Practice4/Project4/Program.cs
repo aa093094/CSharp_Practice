3566584bed589de72bccc88f4183ca7e6465f6a2
Jihoon Choi <aa09302003@gmail.com>
2024-03-19 14:31:11 +0900

    add new projects

---
+++ Practice4/Project4/Program.cs
@@ -0,0 +1,13 @@
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+string[] items = pangram.Split(' ');
+string result = "";
+
+foreach (string item in items)
+{
+    char[] valueArray = item.ToCharArray();
+    Array.Reverse(valueArray);
+    string value = new string(valueArray);
+    result = result + " " + value;
+}
+
+Console.WriteLine(result);
