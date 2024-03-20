495f0d1f648868f5cdda56ff908454b9cac8ab7f
Jihoon Choi <aa09302003@gmail.com>
2024-03-20 15:55:15 +0900

    add new projects

---
+++ Practice4/Project7/Program.cs
@@ -0,0 +1,34 @@
+﻿string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+Console.WriteLine($"Searching THIS Message: {message}");
+char[] openSymbols = { '[', '{', '(' };
+int closingPosition = 0;
+
+while (true)
+{
+    int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+
+    if (openingPosition == -1) break;
+
+    string currentSymbol = message.Substring(openingPosition, 1);
+
+    char matchingSymbol = ' ';
+
+    switch (currentSymbol)
+    {
+        case "[":
+            matchingSymbol = ']';
+            break;
+        case "{":
+            matchingSymbol = '}';
+            break;
+        case "(":
+            matchingSymbol = ')';
+            break;
+    }
+
+    openingPosition += 1;
+    closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
+}
---
+++ Practice4/Project8/Program.cs
@@ -0,0 +1,28 @@
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+string openingWord = "<span>";
+string closingWord = "</span>";
+int openingLength = openingWord.Length;
+ 
+int openingPosition = input.IndexOf(openingWord) + openingLength;
+int closingPosition = input.IndexOf(closingWord);
+int length = closingPosition - openingPosition;
+quantity = input.Substring(openingPosition, length); 
+
+openingWord = "<div>";
+closingWord = "</div>";
+string targetWord = "&trade;";
+string replaceWord = "&reg;";
+openingLength = openingWord.Length;
+
+openingPosition = input.IndexOf(openingWord) + openingLength;
+closingPosition = input.IndexOf(closingWord);
+length = closingPosition - openingPosition;
+output = input.Substring(openingPosition, length);
+output = output.Replace(targetWord, replaceWord);
+
+Console.WriteLine("Quantity: " + quantity);
+Console.WriteLine("Output: " + output);
