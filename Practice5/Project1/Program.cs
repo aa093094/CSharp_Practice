a1c593311b32110d2e841309643162ba8310987b
Jihoon Choi <aa09302003@gmail.com>
2024-03-21 13:09:46 +0900

    add new project

--- Practice5/Project1/Program.cs
+++ Practice5/Project1/Program.cs
@@ -1,13 +1,68 @@
-﻿int[] a = {1, 2, 3, 4, 5};
+﻿/*
+if ipAddress consists of 4 numbers
+and
+if each ipAddress number has no leading zeroes
+and
+if each ipAddress number is in range 0 - 255
 
-Console.WriteLine("Contents of Array:");
-PrintArray();
+then ipAddress is valid
 
-void PrintArray()
+else ipAddress is invalid
+*/
+string[] ipv4Input = { "107.31.1.5", "255.0.0.255", "555..0.555", "255...2555" };
+string[] address;
+bool validLength = false;
+bool validZeroes = false;
+bool validRange = false;
+
+foreach (string ip in ipv4Input)
 {
-    foreach (int x in a)
+    address = ip.Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+    ValidateLength();
+    ValidateZeroes();
+    ValidateRange();
+
+    if (validLength && validZeroes && validRange)
     {
-        Console.Write($"{x} ");
+        Console.WriteLine($"{ip} is a valid IPv4 address");
     }
-    Console.WriteLine();
+    else
+    {
+        Console.WriteLine($"{ip} is an invalid IPv4 address");
+    }
+}
+
+void ValidateLength()
+{
+    validLength = address.Length == 4;
+}
+
+void ValidateZeroes()
+{
+    foreach (string number in address)
+    {
+        if (number.Length > 1 && number.StartsWith("0"))
+        {
+            validZeroes = false;
+            return ;
+        }
+    }
+
+    validZeroes = true;
+}
+
+void ValidateRange()
+{
+    foreach (string number in address)
+    {
+        int value = int.Parse(number);
+        if (value < 0 || value > 255)
+        {
+            validRange = false;
+            return;
+        }
+    }
+
+    validRange = true;
 }
