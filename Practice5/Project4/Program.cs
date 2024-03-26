f26d5ab2bb5646d0621806630cb4c1c628bc7c57
Jihoon Choi <aa09302003@gmail.com>
2024-03-26 14:25:40 +0900

    add new projects

---
+++ Practice5/Project4/Program.cs
@@ -0,0 +1,40 @@
+﻿string[,] corporate = 
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+    DisplayEmail("corporate", i);
+}
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+    DisplayEmail("external", i);
+}
+
+void DisplayEmail(string where, int i)
+{
+    if (where == "corporate")
+    {
+        string name = corporate[i, 0].Substring(0, 2).ToLower();
+        string lastName = corporate[i, 1].ToLower();
+        Console.WriteLine($"{name}{lastName}@contoso.com");
+    }
+    else if (where == "external")
+    {
+        string name = external[i, 0].Substring(0, 2).ToLower();
+        string lastName = external[i, 1].ToLower();
+        Console.WriteLine($"{name}{lastName}@{externalDomain}");
+    }
+}
